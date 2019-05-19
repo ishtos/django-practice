bba948c0affbb5cb78dfdcdf0c02b1ea691a6726
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-05-19 18:19:50 +0900

    change related to env

--- trading/virtual_currency/views.py
+++ trading/virtual_currency/views.py
@@ -1,6 +1,31 @@
-from django.views import generic
+import os
+from dotenv import load_dotenv
 
+from binance.client import Client
 
-class HomeView(generic.TemplateView):
+from django.views.generic import TemplateView
+
+# =============================================================================
+# init
+# =============================================================================
+DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env') 
+load_dotenv(DOTENV_PATH)
+
+API_KEY = os.environ.get("API_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+client = Client(API_KEY, SECRET_KEY)
+
+# =============================================================================
+# view
+# =============================================================================
+class HomeView(TemplateView):
     template_name = 'virtual_currency/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Currency"
+        prices = client.get_all_tickers()
+        context["prices"] = prices
+
+        return context
